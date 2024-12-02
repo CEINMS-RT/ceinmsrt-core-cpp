4f67dfb0a1fb4eb9051893a27e6ac1e9be57dfbb
rogervankanten <roger.van.kanten@gmail.com>
2024-12-02 16:19:14 +0100

    Update header

--- src/Calibrate_MPI.cpp
+++ src/Calibrate_MPI.cpp
@@ -1,11 +1,14 @@
-// This is part of
-// NeuroMuscoloSkeletal Model Software (NMS)
-// Copyright (C) 2010 David Lloyd Massimo Sartori Monica Reggiani
+// This source code is part of:
 //
-// ?? Licenza ??
+// "CEINMS-RT: an open-source framework for the continuous neuro-mechanical model-based control of wearable robots".
+// Copyright (C) 2024 Massimo Sartori, Mohamed Irfan Refai, Lucas Avanci Gaudio, Christopher Pablo Cop, Donatella Simonetti, Federica Damonte, David G. Lloyd, Claudio Pizzolato, Guillaume Durandau.
 //
-// The authors may be contacted via:
-// email: massimo.sartori@gmail.com monica.reggiani@gmail.com
+// CEINMS-RT is an open source software, regulated by the license as described here: https://github.com/CEINMS-RT/ceinmsrt-core-cpp/blob/main/LICENSE
+//
+// The methododologies and ideas implemented in this code are described in the manuscripts below, which should be cited in all publications making use of this code:
+//
+// Massimo Sartori, Mohamed Irfan Refai, Lucas Avanci Gaudio, Christopher Pablo Cop, Donatella Simonetti, Federica Damonte, David G. Lloyd, Claudio Pizzolato, Guillaume Durandau., (2024) "CEINMS-RT: an open-source framework for the continuous neuro-mechanical model-based control of wearable robots"
+//
 
 #include <iostream>
 using std::cout;

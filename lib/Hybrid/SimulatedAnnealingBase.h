c3d1f9d0609acc12c000724ede9d106260e0bea5
rogervankanten <roger.van.kanten@gmail.com>
2024-12-12 13:11:44 +0100

    Final header changes

--- lib/Hybrid/SimulatedAnnealingBase.h
+++ lib/Hybrid/SimulatedAnnealingBase.h
@@ -3,11 +3,11 @@
 // "CEINMS-RT: an open-source framework for the continuous neuro-mechanical model-based control of wearable robots".
 // Copyright (C) 2024 Massimo Sartori, Mohamed Irfan Refai, Lucas Avanci Gaudio, Christopher Pablo Cop, Donatella Simonetti, Federica Damonte, David G. Lloyd, Claudio Pizzolato, Guillaume Durandau.
 //
-// CEINMS-RT is an open source software, regulated by the license as described here: https://github.com/CEINMS-RT/ceinmsrt-core-cpp/blob/main/LICENSE
+// CEINMS-RT is an open source software. Any changes to this code, should be shared back in the open repository: https://github.com/CEINMS-RT. See license as described here: https://github.com/CEINMS-RT/ceinmsrt-core-cpp/blob/main/LICENSE.
 //
-// The methododologies and ideas implemented in this code are described in the manuscripts below, which should be cited in all publications making use of this code:
+// The methodologies and ideas implemented in this code are described in the manuscripts below, which should be cited in all publications making use of this code:
 //
-// Massimo Sartori, Mohamed Irfan Refai, Lucas Avanci Gaudio, Christopher Pablo Cop, Donatella Simonetti, Federica Damonte, David G. Lloyd, Claudio Pizzolato, Guillaume Durandau., (2024) "CEINMS-RT: an open-source framework for the continuous neuro-mechanical model-based control of wearable robots"
+// Massimo Sartori, Mohamed Irfan Refai, Lucas Avanci Gaudio, Christopher Pablo Cop, Donatella Simonetti, Federica Damonte, David G. Lloyd, Claudio Pizzolato, Guillaume Durandau., (2024) "CEINMS-RT: an open-source framework for the continuous neuro-mechanical model-based control of wearable robots. TechRxiv. DOI: 10.36227/techrxiv.173397962.28177284/v1"
 //
 
 #ifndef SimulatedAnnealingBase_h

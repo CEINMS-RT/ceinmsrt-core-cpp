1446673ac356169efd2185bc3625b8365da8642f
Roger van Kanten <roger.van.kanten@gmail.com>
2024-12-12 13:35:54 +0100

    Header (#2)
    * Update header
    
    * Final header changes

--- lib/Calibration/SimulatedAnnealingSpan_parallel.h
+++ lib/Calibration/SimulatedAnnealingSpan_parallel.h
@@ -1,11 +1,14 @@
-// This is part of
-// NeuroMuscoloSkeletal Model Software (NMS)
-// Copyright (C) 2010 David Lloyd Massimo Sartori Monica Reggiani
+// This source code is part of:
 //
-// ?? Licenza ??
+// "CEINMS-RT: an open-source framework for the continuous neuro-mechanical model-based control of wearable robots".
+// Copyright (C) 2024 Massimo Sartori, Mohamed Irfan Refai, Lucas Avanci Gaudio, Christopher Pablo Cop, Donatella Simonetti, Federica Damonte, David G. Lloyd, Claudio Pizzolato, Guillaume Durandau.
 //
-// The authors may be contacted via:
-// email: massimo.sartori@gmail.com monica.reggiani@gmail.com
+// CEINMS-RT is an open source software. Any changes to this code, should be shared back in the open repository: https://github.com/CEINMS-RT. See license as described here: https://github.com/CEINMS-RT/ceinmsrt-core-cpp/blob/main/LICENSE.
+//
+// The methodologies and ideas implemented in this code are described in the manuscripts below, which should be cited in all publications making use of this code:
+//
+// Massimo Sartori, Mohamed Irfan Refai, Lucas Avanci Gaudio, Christopher Pablo Cop, Donatella Simonetti, Federica Damonte, David G. Lloyd, Claudio Pizzolato, Guillaume Durandau., (2024) "CEINMS-RT: an open-source framework for the continuous neuro-mechanical model-based control of wearable robots. TechRxiv. DOI: 10.36227/techrxiv.173397962.28177284/v1"
+//
 
 #ifndef SimulatedAnnealingSpan_h
 #define SimulatedAnnealingSpan_h
--- lib/NMSmodel/SetupDataStructure.h
+++ lib/NMSmodel/SetupDataStructure.h
@@ -1,18 +1,14 @@
 // This source code is part of:
-// "Calibrated EMG-Informed Neuromusculoskeletal Modeling (CEINMS) Toolbox". 
-// Copyright (C) 2015 David G. Lloyd, Monica Reggiani, Massimo Sartori, Claudio Pizzolato
 //
-// CEINMS is not free software. You can not redistribute it without the consent of the authors.
-// The recipient of this software shall provide the authors with a full set of software, 
-// with the source code, and related documentation in the vent of modifications and/or additional developments to CEINMS. 
+// "CEINMS-RT: an open-source framework for the continuous neuro-mechanical model-based control of wearable robots".
+// Copyright (C) 2024 Massimo Sartori, Mohamed Irfan Refai, Lucas Avanci Gaudio, Christopher Pablo Cop, Donatella Simonetti, Federica Damonte, David G. Lloyd, Claudio Pizzolato, Guillaume Durandau.
 //
-// The methododologies and ideas implemented in this code are described in the manuscripts below, which should be cited in all publications making use of this code:
-// Sartori M., Reggiani M., Farina D., Lloyd D.G., (2012) "EMG-Driven Forward-Dynamic Estimation of Muscle Force and Joint Moment about Multiple Degrees of Freedom in the Human Lower Extremity," PLoS ONE 7(12): e52618. doi:10.1371/journal.pone.0052618
-// Sartori M., Farina D., Lloyd D.G., (2014) “Hybrid neuromusculoskeletal modeling to best track joint moments using a balance between muscle excitations derived from electromyograms and optimization,” J. Biomech., vol. 47, no. 15, pp. 3613–3621, 
+// CEINMS-RT is an open source software. Any changes to this code, should be shared back in the open repository: https://github.com/CEINMS-RT. See license as described here: https://github.com/CEINMS-RT/ceinmsrt-core-cpp/blob/main/LICENSE.
 //
-// Please, contact the authors to receive a copy of the "non-disclosure" and "material transfer" agreements:
-// email: david.lloyd@griffith.edu.au, monica.reggiani@gmail.com, massimo.srt@gmail.com, claudio.pizzolato.uni@gmail.com 
-
+// The methodologies and ideas implemented in this code are described in the manuscripts below, which should be cited in all publications making use of this code:
+//
+// Massimo Sartori, Mohamed Irfan Refai, Lucas Avanci Gaudio, Christopher Pablo Cop, Donatella Simonetti, Federica Damonte, David G. Lloyd, Claudio Pizzolato, Guillaume Durandau., (2024) "CEINMS-RT: an open-source framework for the continuous neuro-mechanical model-based control of wearable robots. TechRxiv. DOI: 10.36227/techrxiv.173397962.28177284/v1"
+//
 
 #ifndef SetupDataStructure_
 #define SetupDataStructure_
